5d3b590870081d21681d7e1df9b89d3b9200e160
天空Blond <hurui200320@skyblond.info>
2021-07-05 09:46:50 +0800

    Add router link to wca-details page, add simple data to wca-details.
    Signed-off-by: 天空Blond <hurui200320@skyblond.info>

--- src/app/components/wca-detail/wca-detail.component.ts
+++ src/app/components/wca-detail/wca-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WCA} from '../../models/wca';
+import {WcaService} from '../../services/wca.service';
+import {ActivatedRoute} from '@angular/router';
+import {WalletConnectService} from '../../services/walletconnect.service';
+import {wallet} from '@cityofzion/neon-js';
 
 @Component({
   selector: 'app-wca-detail',
@@ -6,10 +11,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wca-detail.component.scss']
 })
 export class WcaDetailComponent implements OnInit {
+  wca: WCA | null = null;
+  purchasedAmount = 0;
+  isOwner = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private readonly wcaService: WcaService,
+    public readonly walletService: WalletConnectService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.route.params.subscribe((param) => {
+      const identifier = param.id;
+      this.wcaService.queryWCA(identifier).subscribe((result) => {
+        this.wca = result;
+        if (this.walletService.address$.getValue() == null) {
+          this.walletService.address$.subscribe((address) => {
+            this.refreshAddress(address);
+          });
+        } else {
+          this.refreshAddress(this.walletService.address$.getValue());
+        }
+      });
+    });
+  }
+
+  refreshAddress(address: string): void {
+    if (this.wca != null) {
+      this.isOwner = this.wca.creator === address;
+      if (address != null && !this.isOwner) {
+        this.wcaService.queryPurchase(this.wca.identifier, wallet.getScriptHashFromAddress(address))
+          .subscribe((amount) => this.purchasedAmount = amount);
+      }
+    }
+  }
+
+  formatDuration(ms: number): string {
+    if (ms < 0) {
+      ms = -ms;
+    }
+    const time = {
+      day: Math.floor(ms / 86400000),
+      hour: Math.floor(ms / 3600000) % 24,
+      minute: Math.floor(ms / 60000) % 60,
+      second: Math.floor(ms / 1000) % 60,
+      millisecond: Math.floor(ms) % 1000
+    };
+    return Object.entries(time)
+      .filter(val => val[1] !== 0)
+      .map(([key, val]) => `${val} ${key}${val !== 1 ? 's' : ''}`)
+      .join(', ');
+  }
 }
