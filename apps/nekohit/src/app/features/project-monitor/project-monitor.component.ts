469dd55921706b76cd55f57919c990c101bfc59b
ykoitzsch <y.koitzsch@gmx.at>
2021-10-28 00:28:01 +0200

    add and improve milestone overview

--- apps/nekohit/src/app/features/project-monitor/project-monitor.component.ts
+++ apps/nekohit/src/app/features/project-monitor/project-monitor.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { NekoHitProject } from '../../shared/models/project.model';
 
 interface ProjectTimeline {
-  date?: Date;
+  date: Date;
   text: string;
+  subtext?: string;
+  color: string;
+  showDate: boolean;
+  borderColor?: string;
+  class?: string;
 }
 
-interface RiskLevel {
-  text: string;
-  color: string;
-  icon: string;
-}
 @Component({
   selector: 'ritmin-project-monitor',
   templateUrl: './project-monitor.component.html',
@@ -38,22 +38,42 @@ export class ProjectMonitorComponent implements OnInit {
       thresholdMilestoneIndex: 2,
       milestones: [
         {
-          description: '',
+          description:
+            'Some description to the milestone. Some description to the milestone',
           linkToResult: '',
           title: 'dsfsdf',
           endTimestamp: new Date(2021, 11, 11),
         },
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'ccc',
           endTimestamp: new Date(2022, 1, 12),
         },
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'aaa',
           endTimestamp: new Date(2022, 3, 5),
+        },
+        {
+          description:
+            'Some description to the milestone. Some description to the milestone',
+          linkToResult: '',
+          title: 'dsfsdf',
+          endTimestamp: new Date(2022, 4, 5),
+        },
+        {
+          description: 'Some description to the milestone',
+          linkToResult: '',
+          title: 'ccc',
+          endTimestamp: new Date(2022, 5, 5),
+        },
+        {
+          description: 'Some description to the milestone',
+          linkToResult: '',
+          title: 'aaa',
+          endTimestamp: new Date(2022, 6, 5),
         },
       ],
     },
@@ -77,19 +97,19 @@ export class ProjectMonitorComponent implements OnInit {
       thresholdMilestoneIndex: 2,
       milestones: [
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'sdfsdf',
           endTimestamp: new Date(2021, 11, 11),
         },
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'fsdfasfd',
           endTimestamp: new Date(2022, 1, 12),
         },
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'asdfasfd',
           endTimestamp: new Date(2022, 12, 5),
@@ -116,19 +136,19 @@ export class ProjectMonitorComponent implements OnInit {
       thresholdMilestoneIndex: 2,
       milestones: [
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'ffff',
           endTimestamp: new Date(2021, 11, 11),
         },
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'adsfasdfasdfas',
           endTimestamp: new Date(2022, 1, 12),
         },
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'asdf',
           endTimestamp: new Date(2022, 12, 5),
@@ -155,19 +175,20 @@ export class ProjectMonitorComponent implements OnInit {
       thresholdMilestoneIndex: 2,
       milestones: [
         {
-          description: '',
+          description:
+            'Some description to the milestone. Some description to the milestone',
           linkToResult: '',
           title: 'finish abc',
           endTimestamp: new Date(2021, 11, 11),
         },
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'finish y',
           endTimestamp: new Date(2022, 1, 12),
         },
         {
-          description: '',
+          description: 'Some description to the milestone',
           linkToResult: '',
           title: 'finish x',
           endTimestamp: new Date(2022, 12, 5),
@@ -178,54 +199,71 @@ export class ProjectMonitorComponent implements OnInit {
 
   public getProjectTimeline(project: NekoHitProject): ProjectTimeline[] {
     const timeline: ProjectTimeline[] = [];
-    timeline.push({ date: project.creationTimestamp, text: 'Start' });
+    /* add all milestones */
     timeline.push(
       ...project.milestones.map((ms) => ({
         date: ms.endTimestamp,
         text: ms.title,
+        color: 'white',
+        borderColor: '#2196f3',
+        class: 'timeline-event-marker',
+        subtext: '100% Refund',
+        showDate: true,
       }))
     );
-    timeline.push({ text: 'End' });
-    return timeline;
-  }
+    /* update color for finished milestones */
+    timeline
+      .filter((tl, i) => i < project.nextMilestone)
+      .forEach((tl) => (tl.color = '#2196f3'));
 
-  public getRiskLevelForProject(project: NekoHitProject): RiskLevel {
-    if (project.stakePer100Token < 10) {
-      return {
-        text: 'very high risk',
-        color: 'darkred',
-        icon: 'pi pi-angle-double-down',
-      };
-    } else if (project.stakePer100Token <= 30) {
-      return {
-        text: 'high risk',
-        color: 'red',
-        icon: 'pi pi-angle-double-down',
-      };
-    } else if (project.stakePer100Token <= 50) {
-      return {
-        text: 'moderate risk',
-        color: 'darkred',
-        icon: 'pi pi-angle-double-down',
-      };
-    } else if (project.stakePer100Token <= 75) {
-      return {
-        text: 'low risk',
-        color: 'darkred',
-        icon: 'pi pi-angle-double-down',
-      };
-    } else if (project.stakePer100Token <= 100) {
-      return {
-        text: 'very low risk',
-        color: 'darkred',
-        icon: 'pi pi-angle-double-down',
-      };
-    } else
-      return {
-        text: 'no risk',
-        color: 'darkred',
-        icon: 'pi pi-angle-double-down',
-      };
+    /* calculate refund amount */
+
+    const totalMilestones = project.milestones.length;
+    timeline.forEach((tl, i) => {
+      if (i >= project.thresholdMilestoneIndex) {
+        const remainingMs = totalMilestones - i - 1;
+        const result = (remainingMs / totalMilestones) * 100;
+        const rounded = Math.round(result * 100) / 100;
+        tl.subtext = String(rounded + '% refund');
+      }
+    });
+
+    /* add a marker for current status */
+    const currentLabel = 'Current Status';
+    const nextMilestoneDate =
+      timeline[project.nextMilestone].date?.getTime() - 1;
+    timeline.push({
+      text: currentLabel,
+      date: new Date(nextMilestoneDate),
+      color: '#00e599',
+      borderColor: '#00e599',
+      class: 'timeline-event-marker',
+      showDate: false,
+    });
+    timeline.sort(function compare(a, b) {
+      const dateA = new Date(a.date || new Date());
+      const dateB = new Date(b.date || new Date());
+      return dateA.getTime() - dateB.getTime();
+    });
+
+    /* add end to timeline */
+    timeline.push({
+      text: 'End',
+      color: 'white',
+      borderColor: '#2196f3',
+      class: 'timeline-event-marker',
+      showDate: false,
+      date: new Date(),
+    });
+    /* add start to timeline */
+    timeline.unshift({
+      date: project.creationTimestamp,
+      text: 'Start',
+      color: '#2196f3',
+      class: 'timeline-event-marker',
+      showDate: false,
+    });
+    return timeline;
   }
 
   constructor() {}
