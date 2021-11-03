2dcabc015e17172223a2a0b22318a2eef2f262a8
天空Blond <hurui200320@skyblond.info>
2021-11-03 20:38:23 +0800

    Update to new contract
    Signed-off-by: 天空Blond <hurui200320@skyblond.info>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  testNetWcaContractHash: '0x199cd12a70bc554f7d3b0b91c5069546b15c0129',
+  testNetWcaContractHash: '0x514e4dc6398ba12a8c3a5ed96187d606998c4d93',
   testNetCatTokenHash: '0xf461dff74f454e5016421341f115a2e789eadbd7',
   mainNetWcaContractHash: '', // TODO
   mainNetCatTokenHash: '', // TODO
