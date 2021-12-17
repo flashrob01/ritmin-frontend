21ff90ab389f8b21da8f199ffcce1bb3c210d12c
ykoitzsch <y.koitzsch@gmx.at>
2021-12-17 06:44:49 +0100

    fix tokenNumber pipe issue

--- apps/nekohit/src/app/shared/pipes/tokenNumber.pipe.ts
+++ apps/nekohit/src/app/shared/pipes/tokenNumber.pipe.ts
@@ -1,19 +1,24 @@
 import { DecimalPipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
+import { TokenService } from '../../core/services/token.service';
 
 @Pipe({
   name: 'tokenNumber',
 })
 export class TokenNumber implements PipeTransform {
-  constructor(private decimalPipe: DecimalPipe) {}
+  constructor(
+    private decimalPipe: DecimalPipe,
+    private tokenService: TokenService
+  ) {}
   transform(value: number, symbol: string | undefined) {
-    if (symbol === 'CAT') {
-      value = value / 100;
-      return this.decimalPipe.transform(value, '1.2-2');
-    } else if (symbol === 'GAS') {
-      value = value / 100000000;
-      return this.decimalPipe.transform(value, '1.8-8');
+    if (symbol === undefined) {
+      return;
     }
-    throw Error('unknown symbol provided');
+    const token = this.tokenService.getTokenBySymbol(symbol);
+    value = value / Math.pow(10, token.decimals);
+    return this.decimalPipe.transform(
+      value,
+      '1.' + token.decimals + '-' + token.decimals
+    );
   }
 }
