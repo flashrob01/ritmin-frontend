9efeec9b6ff66ce5ab3b93a4ec33ff9192072600
ykoitzsch <y.koitzsch@gmx.at>
2021-12-19 20:31:13 +0100

    improve project dashboard

--- apps/nekohit/src/app/features/project-details/project-details.component.ts
+++ apps/nekohit/src/app/features/project-details/project-details.component.ts
@@ -9,6 +9,16 @@ interface ProjectDetailsState {
   project: NekoHitProject;
 }
 
+interface ProjectTimeline {
+  date: Date;
+  text: string;
+  subtext?: string;
+  color: string;
+  showDate: boolean;
+  borderColor?: string;
+  class?: string;
+}
+
 @Component({
   selector: 'ritmin-project-details',
   templateUrl: './project-details.component.html',
@@ -50,5 +60,84 @@ export class ProjectDetailsComponent {
     };
     project.stakedTokensChartData = data;
     return project;
+  }
+
+  public getProjectTimeline(project: NekoHitProject): ProjectTimeline[] {
+    const timeline: ProjectTimeline[] = [];
+    /* add all milestones */
+    timeline.push(
+      ...project.milestones.map((ms) => ({
+        date: ms.endTimestamp,
+        text: ms.title,
+        color: 'white',
+        borderColor: '#2196f3',
+        class: 'timeline-event-marker',
+        subtext: '100% Refund',
+        showDate: true,
+      }))
+    );
+    /* update color for finished milestones */
+    timeline
+      .filter((tl, i) => i < project.nextMilestone)
+      .forEach((tl) => (tl.color = '#2196f3'));
+
+    /* calculate refund amount */
+
+    const totalMilestones = project.milestonesCount;
+    timeline.forEach((tl, i) => {
+      if (i >= project.thresholdMilestoneIndex) {
+        const remainingMs = totalMilestones - i - 1;
+        const result = (remainingMs / totalMilestones) * 100;
+        const rounded = Math.round(result * 100) / 100;
+        tl.subtext = String(rounded + '% refund');
+      }
+    });
+
+    /* add a marker for current status if project is not finished*/
+    if (project.status !== 'FINISHED') {
+      const currentLabel = 'Current Status';
+      let nextMilestoneDate;
+      if (project.nextMilestone !== project.milestonesCount) {
+        nextMilestoneDate = timeline[project.nextMilestone].date?.getTime() - 1;
+      } else {
+        nextMilestoneDate =
+          timeline[project.nextMilestone - 1].date.getTime() + 1;
+      }
+      timeline.push({
+        text: currentLabel,
+        date: new Date(nextMilestoneDate),
+        color: '#00e599',
+        borderColor: '#00e599',
+        class: 'timeline-event-marker',
+        showDate: false,
+      });
+    }
+
+    timeline.sort(function compare(a, b) {
+      const dateA = new Date(a.date || new Date());
+      const dateB = new Date(b.date || new Date());
+      return dateA.getTime() - dateB.getTime();
+    });
+
+    const finishMarkerColor =
+      project.status === 'FINISHED' ? '#2196f3' : 'white';
+    /* add end to timeline */
+    timeline.push({
+      text: 'End',
+      color: finishMarkerColor,
+      borderColor: '#2196f3',
+      class: 'timeline-event-marker',
+      showDate: false,
+      date: new Date(),
+    });
+    /* add start to timeline */
+    timeline.unshift({
+      date: project.creationTimestamp,
+      text: 'Start',
+      color: '#2196f3',
+      class: 'timeline-event-marker',
+      showDate: false,
+    });
+    return timeline;
   }
 }
